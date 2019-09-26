ed01e9f87fc360965bac789eab1d95f49b79a7d2
sienna <tune9004@gmail.com>
2019-09-26 14:37:55 +0900

    아이콘추가
    아이콘 나중에 변경할예정 ㅜ

---
+++ 01_ANDROID/190926_CARPIC_V1.4(아이콘변경)/CarPic_3/app/src/main/java/com/example/carpic_3/SplashActivity.java
@@ -0,0 +1,52 @@
+package com.example.carpic_3;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Handler;
+import android.util.Log;
+//import java.util.logging.Handler;
+
+
+/*[로딩화면 Activity]*/
+public class SplashActivity extends AppCompatActivity {
+
+    private static final String TAG = "RegisterActivity";
+
+    private Handler mHandler;
+    private Runnable mRunnable;
+
+
+    @Override
+    protected void onCreate(@NonNull Bundle savedInstanceState) {
+
+        Log.e(TAG, "onCreate started in SplashActivity");
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        mRunnable = new Runnable() {
+            @Override
+            public void run() {
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            }
+        };
+
+        mHandler = new Handler();
+        mHandler.postDelayed(mRunnable,3000);
+
+        Log.e(TAG, "onCreate ended in SplashActivity");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mHandler != null)
+        {
+            mHandler.removeCallbacks(mRunnable);
+        }
+    }
+}
